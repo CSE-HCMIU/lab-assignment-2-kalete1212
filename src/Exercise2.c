d7c11d12cf3cf576dc5c1089da23e15508a2510f
kalete1212 <ITITIU20254@student.hcmiu.edu.vn>
2021-10-11 16:41:29 +0700

    Update Exercise2.c

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -4,7 +4,7 @@ which satisfies the following:
 	if n is odd, factorial = 1 * 3 * 5 * … * n
 	if n is even, factorial = 2 * 4 * 6 * … * n
 Ex:
-______________________________________
+____________________________________
 | Input: 11                          |
 | Output: 10395                      |
 |____________________________________|
@@ -17,21 +17,31 @@ ______________________________________
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int n = atoi(argv[1]);
-	int result;
+	int result1, result2;
 	//Your codes here
 	int i;
-	if (n % 2 ==1 )
-	{
-		i=1;
-	}
-	else
-	{
-		i=2;
-	}
-	for (i;i<=n;i+=2)
-	result *= i;
-	
-
-	printf("%d",result);
-	return 0;
+    result1 = 1;
+    result2 = 1;
+    i = 2;
+    while(i < n+1)
+    {
+        if(i % 2 == 0)
+        {
+            result2 *= i;
+        }
+        else
+      {
+        result1 *= i;
+      }
+        i++;
+    }
+    if (i % 2 ==0)
+    {
+    printf("%d", result1);
+    }
+    else {
+    printf("%d", result2);
+    }
+    getc;
+    return 0;
 }
